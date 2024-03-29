3b9b22768c474d0ce968f911a9e089e0d7e3f214
Adam Stępień <vesemir2k@gmail.com>
2024-03-29 22:37:07 +0100

    Add styles for PlanetHeading

--- src/components/planet/PlanetHeading/index.jsx
+++ src/components/planet/PlanetHeading/index.jsx
@@ -1,13 +1,13 @@
-import { description, heading, source, title } from './PlanetHeading.module.css';
+import PlanetHeadeingCss from './PlanetHeading.module.css';
 
 const PlanetHeading = ({ sectionPlanetData, namePlanet }) => {
   return (
-    <div className={heading}>
-      <h1 className={title}>{namePlanet}</h1>
-      <p className={description}>{sectionPlanetData.content}</p>
-      <span className={source}>
-        Source:
-        <a href={sectionPlanetData.source}>
+    <div className={PlanetHeadeingCss.heading}>
+      <h1 className={PlanetHeadeingCss.title}>{namePlanet}</h1>
+      <p className={PlanetHeadeingCss.description}>{sectionPlanetData.content}</p>
+      <span className={PlanetHeadeingCss.source}>
+        Source :{' '}
+        <a className={PlanetHeadeingCss.wikipedia} href={sectionPlanetData.source}>
           Wikipedia <img src="./assets/icon-source.svg" alt="" />
         </a>
       </span>
