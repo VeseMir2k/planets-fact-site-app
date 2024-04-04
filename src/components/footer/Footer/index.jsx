59475744feb23e932f186cd596e3c3fb84e449cf
Adam Stępień <vesemir2k@gmail.com>
2024-04-05 01:09:47 +0200

    Add styles for footer

--- src/components/footer/Footer/index.jsx
+++ src/components/footer/Footer/index.jsx
@@ -2,7 +2,11 @@ import FooterCSS from './Footer.module.css';
 
 const Footer = () => (
   <footer className={FooterCSS.footer}>
-    <p>@VeseMir2k</p>
+    <div className={FooterCSS.attribution}>
+      Challenge by
+      <a href="https://www.frontendmentor.io?ref=challenge"> Frontend Mentor</a>. Coded by
+      <a href="https://github.com/VeseMir2k"> VeseMir2k</a>.
+    </div>
   </footer>
 );
 
