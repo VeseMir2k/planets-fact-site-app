9117d774e2394bd2d965c7ce9845afa1b28ce8b0
Adam Stępień <vesemir2k@gmail.com>
2024-04-05 01:01:26 +0200

    Add navlink to logo

--- src/components/header/HeaderLogo/index.jsx
+++ src/components/header/HeaderLogo/index.jsx
@@ -1,5 +1,10 @@
+import { NavLink } from 'react-router-dom';
 import LogoCSS from './HeaderLogo.module.css';
 
-const HeaderLogo = () => <h2 className={LogoCSS.logo}>The Planets</h2>;
+const HeaderLogo = () => (
+  <h2 className={LogoCSS.logo}>
+    <NavLink to="/">The Planets</NavLink>
+  </h2>
+);
 
 export default HeaderLogo;
